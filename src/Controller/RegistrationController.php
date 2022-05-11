4cca132c03507fc314730f418c6f66357bc26768
pauline marot <pauline.marot2@outlook.fr>
2022-05-11 08:12:42 +0200

    HeaderLogo

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -80,6 +80,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Votre adresse e-mail a été vérifiée.');//Your email address has been verified
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
