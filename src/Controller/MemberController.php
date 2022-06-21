d622083f847ea0835dd711ccb424af245391e931
TOULLEC <simon-toullec@outlook.fr>
2022-06-21 15:15:41 +0200

    import file csv and insert in bdd

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -6,6 +6,7 @@ use App\Classe\affiliated;
 use App\Entity\Member;
 use App\Form\MemberType;
 use App\Repository\EventRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,6 +18,22 @@ class MemberController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
     private EventRepository $eventRepository;
+
+    const CLUB="CLUB";
+    const NOM="NOM";
+    const PRENOM="PRENOM";
+    const MF="M/F";
+    const NEE_LE="NE(E) LE";
+    const ADRESSE="ADRESSE";
+    const CODE_POSTAL="CODE POSTAL";
+    const VILLE="VILLE";
+    const TEL="TEL";
+    const TEL_ACCIDENT="TEL ACCIDENT";
+    const NATIONALITE="NATIONALITE";
+    const EMAIL="EMAIL";
+    const GRADE="GRADE TAEKWONKIDO";
+    
+    
 
     public function __construct(EntityManagerInterface $entityManager, EventRepository $eventRepository)
     {
@@ -39,25 +56,112 @@ class MemberController extends AbstractController
 
     }
 
+    #[Route('/account/members/export-csv', name: 'account_member_export_csv')] 
+    public function exportCsv(): Response
+    {
+        $adhesion = $this->eventRepository->findActualAdhesion();
+
+        return $this->render('account/member.html.twig', [
+            'adhesion' => $adhesion
+        ]);
+    }
+
     #[Route('/account/members/import-csv', name: 'account_member_import_csv')] 
     public function importCsv(): Response
     {
         $adhesion = $this->eventRepository->findActualAdhesion();
 
-        if ( $xlsx = SimpleXLSX::parse('test.xlsx') ) {
- 
-            foreach( $xlsx->rows() as $key=> $r ) {
-                // $produit= new Produit();
-                // $produit->setRef($r[0]);
-                // $produit->SetPrix($r[1]);
-               
-                // $em->persist($produit);
- 
+        $targetPath = "upload/".basename($_FILES['inpFile']['name']);
+        move_uploaded_file($_FILES['inpFile']['tmp_name'],$targetPath);
+
+        $index = 0;
+        $arrColumn = null;
+        $arrayResult = [];
+        $member = null;
+        
+        if (($fp = fopen($targetPath, "r")) !== FALSE) {
+
+            while (($row = fgetcsv($fp, 1000, ",")) !== FALSE) {
+
+                if($index === 0){
+                    $arrColumn = explode(';',$row[0]);
+                }else{
+                    $arrRow = explode(';',$row[0]);
+
+                    for($c = 0; $c < count($arrRow); $c++){
+                        $arrayResult[$index][$arrColumn[$c]] = $arrRow[$c];
+                    }
+                }
+
+                $index++;
             }
-            // $em->flush();
- 
-        } else {
-            echo SimpleXLSX::parseError();
+            fclose($fp);
+        }
+
+        for($j = 1; $j < count($arrayResult); $j++){
+            $member = new Member();
+
+            foreach($arrayResult[$j] as $key => $value){
+
+                if($key === self::CLUB){
+                    // $member->setClub($value);
+
+                }else if($key === self::NOM){
+                    $member->setLastName($value);
+
+                }else if($key === self::PRENOM){
+                    $member->setFirstName($value);
+
+                }else if($key === self::MF){
+
+                    if($value === "M"){
+                        $member->setSex("Homme");
+
+                    }else{
+                        $member->setSex("Femme");
+            
+                    }
+
+                }else if($key === self::NEE_LE){
+                    $date = DateTime::createFromFormat('d/m/Y', $value);
+                    $strDate = $date->format('Y-m-d');
+                    $resDate = new DateTime($strDate);
+                    $member->setBirthdate($resDate);
+
+                }else if($key === self::ADRESSE){
+                    $member->setStreetAddress($value);
+
+                }else if($key === self::CODE_POSTAL){
+                    $member->setPostalCode($value);
+
+                }else if($key === self::VILLE){
+                    $member->setCity($value);
+
+                }else if($key === self::TEL){
+                    $member->setPhoneNumber($value);
+
+                }else if($key === self::TEL_ACCIDENT){
+                    $member->setEmergencyPhone($value);
+
+                }else if($key === self::NATIONALITE){
+                    $member->setNationality($value);
+
+                }else if($key === self::EMAIL){
+                    $member->setEmail($value);
+
+                }else if($key === self::GRADE){
+                    $member->setLevel($value);
+
+                }
+            }
+
+            $this->entityManager->persist($member);
+
+        }
+        
+
+        if($member){
+            $this->entityManager->flush();
         }
 
         return $this->render('account/member.html.twig', [
@@ -102,7 +206,7 @@ class MemberController extends AbstractController
     #[Route('/account/members-edit/{id}', name: 'account_member_edit')] //affichage du formulaire d'adhesion, de modification du membre
     public function edit(Request $request, $id): Response
     {
-       $member = $this->entityManager->getRepository(Member::class)->findOneById($id);
+        $member = $this->entityManager->getRepository(Member::class)->findOneById($id);
 
         if(!$member || $member->getResponsibleAdult() != $this->getUser()) {
             return $this->redirectToRoute('account_member');
